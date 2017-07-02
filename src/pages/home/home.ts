717ad0b8e747a75dc4bd7723412f93304c1e5a99
WilixLead <lead@w.wilix.ru>
2017-07-02 15:24:19 +0300

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,9 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  myDate: Date;
   constructor(public navCtrl: NavController) {
-
+    this.myDate  = new Date();
   }
 
 }
